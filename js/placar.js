cd12fdfd06598d2e6c9268069ba6346cb5273789
BruLekichLouro <bububio@yahoo.com.br>
2018-10-15 16:51:38 -0300

    Chamando o tooltip após a requisição AJAX para sinalizar que a sincronização ocorreu com sucesso

--- js/placar.js
+++ js/placar.js
@@ -1,35 +1,9 @@
 $("#botao-placar").click(mostraPlacar);
 $("#botao-sync").click(sincronizaPlacar);
 
-function sincronizaPlacar(){
-	var placar = [];
-    var linhas = $("tbody>tr");
-
-    linhas.each(function(){
-        var usuario = $(this).find("td:nth-child(1)").text();
-        var palavras = $(this).find("td:nth-child(2)").text();
-
-        var score = {
-            usuario: usuario,
-            pontos: palavras            
-        };
-
-        placar.push(score);
-
-    });
-
-    var dados = {
-        placar: placar
-    };
-
-    $.post("http://localhost:3000/placar", dados, function(){
-    	console.log("Placar sincronizado com sucesso");
-    });
- }
-
 function inserePlacar (){
 	var corpoTabela = $(".placar").find("tbody");
-	var usuario = "Bruna";
+	var usuario = $("#usuarios").val();
     var numPalavras = $("#contador-palavras").text();
     var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>" ;
 
@@ -38,15 +12,6 @@ function inserePlacar (){
     corpoTabela.append(linha);
     $(".placar").slideDown(500);
     scrollPlacar();
-}
-function atualizaPlacar(){
-	$.get("http://localhost:3000/placar",function(data){
-	$(data).each(function(){
-		var linha = novaLinha(this.usuario, this.pontos);
-		linha.find(".botao-remover").click(removeLinha);
-		$("tbody").append(linha);
-        });
-    });
 }
 function scrollPlacar() {
     var posicaoPlacar = $(".placar").offset().top;
@@ -92,4 +57,45 @@ function removeLinha(){
 function mostraPlacar() {
 	$(".placar").stop().slideToggle(600);
 }
+function atualizaPlacar(){ 
+	$.get("http://localhost:3000/placar",function(data){
+		$(data).each(function(){
+		var linha = novaLinha(this.usuario, this.pontos);
+		linha.find(".botao-remover").click(removeLinha);
+		$("tbody").append(linha);
+        });
+    });
+}
 
+function sincronizaPlacar(){
+	var placar = [];
+    var linhas = $("tbody>tr");
+
+    linhas.each(function(){
+        var usuario = $(this).find("td:nth-child(1)").text();
+        var palavras = $(this).find("td:nth-child(2)").text();
+
+        var score = {
+            usuario: usuario,
+            pontos: palavras            
+        };
+
+        placar.push(score);
+
+    });
+
+    var dados = {
+        placar: placar
+    };
+
+    $.post("http://localhost:3000/placar", dados , function() {
+    	console.log("Placar sincronizado com sucesso");
+    	$(".tooltip").tooltipster("open"); 
+		}).fail(function(){
+    	$(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar"); 
+		}).always(function(){ //novo
+    		setTimeout(function() {
+    		$(".tooltip").tooltipster("close"); 
+		}, 1200);
+ 	});
+ }			
